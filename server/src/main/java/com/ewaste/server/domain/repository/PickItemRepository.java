fc0cc4d53ddf7bc8c5815c2ef8a97aee12bfefd1
ani28-bit <anitapervin868@gmail.com>
2026-09-07 21:37:47 +0600

    PickItemRepo

--- server/src/main/java/com/ewaste/server/domain/repository/PickItemRepository.java
+++ server/src/main/java/com/ewaste/server/domain/repository/PickItemRepository.java
@@ -1,4 +1,13 @@
 package com.ewaste.server.domain.repository;
+import com.ewaste.server.domain.model.PickupItem;
+import java.util.List;
+import java.util.Optional;
 
-public class PickItemRepository {
+public interface PickupItemRepository {
+    PickupItem save(PickupItem pickupItem);
+    Optional<PickupItem> findById(Long pickupItemId);
+    List<PickupItem> findByPickupId(Long pickupId);
+    List<PickupItem> findByItemId(Long itemId);
+    void deleteById(Long pickupItemId);
+    void deleteByPickupId(Long pickupId);
 }
