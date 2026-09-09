63da3a3bb7e593b8a1a667e659cdffe3a5ee8e9a
Nazia Hasan <naziahasan0101@gmail.com>
2026-09-09 13:27:47 +0600

    chore: update encodings and client pom dependencies

---
+++ client/src/main/java/com/ewaste/client/dto/request/EWasteItemClientRequest.java
@@ -0,0 +1,4 @@
+package com.ewaste.client.dto.request;
+
+public class EWasteItemClientRequest {
+}
---
+++ client/src/main/java/com/ewaste/client/dto/response/RecyclingCenterClientResponse.java
@@ -0,0 +1,4 @@
+package com.ewaste.client.dto.response;
+
+public class RecyclingCenterClientResponse {
+}
